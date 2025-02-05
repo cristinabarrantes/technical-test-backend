d84685a172833b52589d4d120ab76e290e547b5b
Cristina Barrantes <cbarrantes@gmail.com>
2025-02-05 08:57:29 +0100

    Removing warnings due to servial version UID.

--- src/main/java/com/playtomic/tests/wallet/service/StripeAmountTooSmallException.java
+++ src/main/java/com/playtomic/tests/wallet/service/StripeAmountTooSmallException.java
@@ -1,4 +1,6 @@
 package com.playtomic.tests.wallet.service;
 
 public class StripeAmountTooSmallException extends StripeServiceException {
+
+  private static final long serialVersionUID = 1L;
 }
--- src/main/java/com/playtomic/tests/wallet/service/StripeServiceException.java
+++ src/main/java/com/playtomic/tests/wallet/service/StripeServiceException.java
@@ -1,4 +1,6 @@
 package com.playtomic.tests.wallet.service;
 
 public class StripeServiceException extends RuntimeException {
+
+  private static final long serialVersionUID = 1L;
 }
