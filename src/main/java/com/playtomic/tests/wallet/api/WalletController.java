40735b66c3551b1d5516243e6aeaa74d86809333
Cristina Barrantes <cbarrantes@gmail.com>
2025-02-05 08:55:40 +0100

    Use lombok to define the log.

--- src/main/java/com/playtomic/tests/wallet/api/WalletController.java
+++ src/main/java/com/playtomic/tests/wallet/api/WalletController.java
@@ -1,13 +1,13 @@
 package com.playtomic.tests.wallet.api;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class WalletController {
-    private Logger log = LoggerFactory.getLogger(WalletController.class);
 
     @RequestMapping("/")
     void log() {
